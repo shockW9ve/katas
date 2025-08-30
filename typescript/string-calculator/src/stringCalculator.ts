7f8f57e6687ce6a1d422e9fff45ad388deb41517
shockW9ve <shockW9ve@gmail.com>
2025-08-30 18:56:29 +0200

    test: added several test

--- typescript/string-calculator/src/stringCalculator.ts
+++ typescript/string-calculator/src/stringCalculator.ts
@@ -1,19 +1,45 @@
 export function add(input: string) {
-  let replace = input.replaceAll(",", "");
-  let trimmed = replace.trim();
+  if (input === undefined || typeof input !== "string" || input === null) {
+    throw new Error(
+      "input can not be undefined, other type than String or null",
+    );
+  }
 
   let sum = 0;
-  for (let i = 0; i < trimmed.length; i++) {
-    if (parseInt(trimmed[i]) < 0) {
+  if (input.length === 0) {
+    return sum;
+  }
+
+  let trimmed = input.trim();
+  if (trimmed.length === 1) {
+    return Number(trimmed);
+  }
+
+  let split: string[] = delimiter(trimmed);
+
+  for (let i = 0; i < split.length; i++) {
+    if (Number(split[i]) < 0) {
       throw new Error("negative numbers are not accepted");
+    } else if (Number(split[i]) > 1000) {
+      continue;
     } else {
-      sum += parseInt(trimmed[i]);
+      sum += Number(split[i]);
     }
   }
 
-  if (input === "") {
-    return 0;
+  return sum;
+}
+
+function delimiter(str: string) {
+  let split: string[] = [];
+
+  if (str.indexOf("\n") >= 0) {
+    split = str.split("\n");
   }
 
-  return sum;
+  if (str.indexOf(",") >= 0) {
+    split = str.split(",");
+  }
+
+  return split;
 }
--- typescript/string-calculator/tests/stringCalculator.spec.ts
+++ typescript/string-calculator/tests/stringCalculator.spec.ts
@@ -12,4 +12,24 @@ describe("stringCalculator.add()", () => {
       console.error(error);
     }
   });
+
+  it("adding two numbers gives a positive number", () => {
+    expect(add("2, 3")).toBe(5);
+  });
+
+  it("nummber above 1000 is ignored", () => {
+    expect(add("1001, 3")).toBe(3);
+  });
+
+  it("single number returns value", () => {
+    expect(add("9")).toBe(9);
+  });
+
+  it("newline acts a delimiter", () => {
+    expect(add("1\n 2")).toBe(3);
+  });
+
+  it("supports mix of newline and comma", () => {
+    expect(add("3\n, 3")).toBe(6);
+  });
 });
