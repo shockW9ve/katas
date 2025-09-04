6072e7eb4cd653841550a81d08f71e74c7e2b3e3
shockW9ve <shockW9ve@gmail.com>
2025-09-04 14:12:13 +0200

    initial csharp setup

---
+++ csharp/StringCalculator/src/StringCalculator/StringCalculator.cs
@@ -0,0 +1,9 @@
+﻿namespace StringCalculator;
+
+public class StringCalculator
+{
+    public int Add(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ csharp/StringCalculator/tests/StringCalculator.Tests/StringCalculator.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+
+namespace StringCalculator.Tests;
+
+public class UnitTest1
+{
+    [Theory]
+    [InlineData("3,6", 9)]
+    [InlineData("3,6,9", 18)]
+    public void Sums_CommaSeperated_Numbers(string input, int expected)
+    {
+        // arrange
+        StringCalculator calculator = new StringCalculator();
+        // act + assert
+        calculator.Add(input).Should().Be(expected);
+    }
+}
