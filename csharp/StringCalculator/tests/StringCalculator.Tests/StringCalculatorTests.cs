d9a0e31b715c0e4c4faa679238b831e20d46fd7b
shockW9ve <shockW9ve@gmail.com>
2025-09-20 17:08:07 +0200

    refactor: DRY added to unit tests

--- csharp/StringCalculator/tests/StringCalculator.Tests/StringCalculatorTests.cs
+++ csharp/StringCalculator/tests/StringCalculator.Tests/StringCalculatorTests.cs
@@ -4,39 +4,38 @@ namespace StringCalculator.Tests;
 
 public class StringCalculatorTests
 {
+    private readonly StringCalculator _sut = new StringCalculator();
+
     [Fact]
     public void EmptyString_Returns_Zero()
     {
+        // arrange
         string EMPTY = "";
         int expected = 0;
-        // arrange
-        StringCalculator calculator = new StringCalculator();
         // act
-        calculator.Add(EMPTY).Should().Be(expected);
+        _sut.Add(EMPTY).Should().Be(expected);
     }
 
     [Fact]
     public void Negative_Numbers_Throws_ArgumentException()
     {
         // arrange 
-        StringCalculator calculator = new StringCalculator();
         // act + assert
         // assert version
         // Assert.Throws<ArgumentException>(() => calculator.Add("-3,6"));
         // fluent assertion method
-        Action action = () => calculator.Add("-3,6");
-        action.Should().Throw<ArgumentException>().WithMessage("Negative numbers are not allowed: -3");
+        Action action = () => _sut.Add("-3,6");
+        action.Should().Throw<ArgumentException>().WithMessage("*Negative numbers*");
     }
 
     [Fact]
     public void Negative_Numbers_Throws_With_Numbers_List()
     {
         // arrange 
-        StringCalculator calculator = new StringCalculator();
         string input = "3,-6,-9";
         // act + assert
-        Action action = () => calculator.Add(input);
-        action.Should().Throw<ArgumentException>().WithMessage("Negative numbers are not allowed: -6,-9");
+        Action action = () => _sut.Add(input);
+        action.Should().Throw<ArgumentException>().WithMessage("*-6,-9*");
     }
 
     [Theory]
@@ -46,9 +45,8 @@ public class StringCalculatorTests
     public void Sums_CommaSeparated_Numbers(string input, int expected)
     {
         // arrange
-        StringCalculator calculator = new StringCalculator();
         // act + assert
-        calculator.Add(input).Should().Be(expected);
+        _sut.Add(input).Should().Be(expected);
     }
 
     [Theory]
@@ -56,10 +54,19 @@ public class StringCalculatorTests
     [InlineData("3,\n3,3", 9)]
     public void Sums_With_Newline_Delimiter(string input, int expected)
     {
-        // arrage 
-        StringCalculator calculator = new StringCalculator();
+        // arrange
         // act + assert
-        calculator.Add(input).Should().Be(expected);
+        _sut.Add(input).Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("1000,1", 1001)]  // 1000 counts
+    [InlineData("1001,2", 2)]     // 1001 ignored
+    public void Handles_Upper_Limit_Boundary(string input, int expected)
+    {
+        // arrange
+        // act + assert
+        _sut.Add(input).Should().Be(expected);
     }
 
     [Theory]
@@ -68,20 +75,26 @@ public class StringCalculatorTests
     public void Uses_Custom_SingleChar_Delimiter(string input, int expected)
     {
         // arrange
-        StringCalculator calculator = new StringCalculator();
         // act + assert
-        calculator.Add(input).Should().Be(expected);
+        _sut.Add(input).Should().Be(expected);
+    }
+
+    [Fact]
+    public void Custom_Delimiter_Other_Than_Semicolon_Works()
+    {
+        // arrange
+        // act + assert
+        _sut.Add("//|\n1|2|3").Should().Be(6);
     }
 
     [Fact]
     public void Adjecent_Delimiter_Do_Not_Crash()
     {
         // arrange
-        StringCalculator calculator = new StringCalculator();
         string input = "1,\n2,3";
         int expected = 6;
         // act + assert
-        calculator.Add(input).Should().Be(expected);
+        _sut.Add(input).Should().Be(expected);
     }
 
 
