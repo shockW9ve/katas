48de41b0dda90ec394ae795d6d941240b9dba0fe
shockW9ve <shockW9ve@gmail.com>
2025-11-22 17:23:00 +0100

    initial setup for new kata

---
+++ csharp/MarsRover/MarsRover.Domain/Models/MarsRover.cs
@@ -0,0 +1,36 @@
+﻿using Space.Helpers;
+
+namespace Space.Models;
+
+public class MarsRover
+{
+    public required int X_Position { get; set; }
+    public required int Y_Position { get; set; }
+    public Enum Compas { get; set; } = Direction.North;
+
+    public string Report()
+    {
+        return $"{X_Position} {Y_Position} {Compas.ToString().Substring(0, 1)}";
+    }
+
+    public void Turn(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.North:
+                break;
+
+            case Direction.East:
+                break;
+
+            case Direction.South:
+                break;
+
+            case Direction.West:
+                break;
+
+            default:
+                break;
+        }
+    }
+}
