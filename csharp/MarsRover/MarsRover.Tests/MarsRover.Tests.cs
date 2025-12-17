f031f1291be8c194cfc7be932d24da158f2f25a2
shockW9ve <shockW9ve@gmail.com>
2025-12-17 19:29:32 +0100

    test: almost done with tests. A few more tweaks and this kata is finished

--- csharp/MarsRover/MarsRover.Tests/MarsRover.Tests.cs
+++ csharp/MarsRover/MarsRover.Tests/MarsRover.Tests.cs
@@ -19,7 +19,7 @@ public class MarsRoverTests
     }
 
     [Fact]
-    public void Turning_Should_Happen_In_90Degrees()
+    public void Turning_Left_Should_Happen_In_90Degrees()
     {
         // arrange 
         var sut = new MarsRover(0, 0);
@@ -29,108 +29,29 @@ public class MarsRoverTests
         // assert
         result.Should().Be(Direction.West);
     }
-    //
-    //
-    // [Fact]
-    // public void Turning_Twice_In_Same_Direction_Should_Happen_In_180Degrees()
-    // {
-    //     // arrange 
-    //     var sut = new MarsRover()
-    //     {
-    //         Y_Position = 0,
-    //         X_Position = 0
-    //     };
-    //
-    //     string direction = "ll";
-    //     // act
-    //     sut.Turn(direction);
-    //     var result = sut.Compas;
-    //     // assert
-    //     result.Should().Be(Direction.South);
-    // }
-    //
-    // [Fact]
-    // public void Turning_Trice_In_Same_Direction_Should_Happen_In_270Degrees()
-    // {
-    //     // arrange 
-    //     var sut = new MarsRover()
-    //     {
-    //         Y_Position = 0,
-    //         X_Position = 0
-    //     };
-    //
-    //     string direction = "rrr";
-    //     // act
-    //     sut.Turn(direction);
-    //     var result = sut.Compas;
-    //     // assert
-    //     result.Should().Be(Direction.West);
-    // }
-    //
+
     [Fact]
-    public void Moving_Forward_When_Facing_North_Should_Plus_Y_With_One()
+    public void Turning_Right_Should_Happen_In_90Degrees()
     {
         // arrange 
         var sut = new MarsRover(0, 0);
-        Position position = new Position(0, 1);
-        //act
-        sut.AdvanceTo(position);
-        int x = sut.Position.X;
-        int y = sut.Position.Y;
-        var direction = sut.Heading;
+        // act
+        sut.TurnRight();
+        var result = sut.Heading;
         // assert
-        x.Should().Be(0);
-        y.Should().Be(1);
-        direction.Should().Be(Direction.North);
+        result.Should().Be(Direction.East);
     }
-    //
-    // [Fact]
-    // public void Moving_Forward_When_Facing_East_Should_Plus_X_With_One()
-    // {
-    //     // arrange 
-    //     var sut = new MarsRover()
-    //     {
-    //         Y_Position = 1,
-    //         X_Position = 1,
-    //         Compas = Direction.East
-    //     };
-    //
-    //     string move = "m";
-    //     //act
-    //     sut.Turn(move);
-    //     int y = sut.Y_Position;
-    //     int x = sut.X_Position;
-    //     var direction = sut.Compas;
-    //     // assert
-    //     y.Should().Be(1);
-    //     x.Should().Be(2);
-    //     direction.Should().Be(Direction.East);
-    // }
-    //
-    // [Fact]
-    // public void Moving_With_Several_Movement_And_Directions_Should_Not_Crash()
-    // {
-    //     // arrange 
-    //     var sut = new MarsRover()
-    //     {
-    //         Y_Position = 2,
-    //         X_Position = 1,
-    //     };
-    //
-    //     string move = "LMLMLMLMM";
-    //     // 1,2,n
-    //     //     0,2,w
-    //     //     0,1,s
-    //     //     1,1,e
-    //     //     1,3,n
-    //     //act
-    //     sut.Turn(move);
-    //     int y = sut.Y_Position;
-    //     int x = sut.X_Position;
-    //     var direction = sut.Compas;
-    //     // assert
-    //     y.Should().Be(3);
-    //     x.Should().Be(1);
-    //     direction.Should().Be(Direction.North);
-    // }
+
+    [Fact]
+    public void Advancing_To_A_New_Position_Should_Reflect_In_Rover_Coordinate()
+    {
+        // arrange 
+        var sut = new MarsRover(0, 0);
+        Position to = new Position(1, 0);
+        // act
+        sut.AdvanceTo(to);
+        // assert
+        sut.Position.X.Should().Be(1);
+        sut.Position.Y.Should().Be(0);
+    }
 }
--- csharp/MarsRover/MarsRover.Tests/RoverService.Tests.cs
+++ csharp/MarsRover/MarsRover.Tests/RoverService.Tests.cs
@@ -25,4 +25,93 @@ public class RoverServiceTests
         // assert
         result.Status.Should().Be(MoveStatus.InvalidCommand);
     }
+
+    [Fact]
+    public void Giving_Only_Turn_Directions_Should_Execute_And_Return_Completed()
+    {
+        // arrange
+        RoverService service = new RoverService();
+        int x = 0;
+        int y = 0;
+        MarsRover rover = new MarsRover(x, y);
+        int height = 5;
+        int width = 5;
+        IReadOnlySet<Position> obstacles = new HashSet<Position>();
+        Plateau plateau = new Plateau(height, width, obstacles);
+        string commands = "lrlr";
+        // act
+        var result = service.Execute(rover, plateau, commands);
+        // assert
+        result.Status.Should().Be(MoveStatus.Completed);
+    }
+
+    [Fact]
+    public void Giving_Several_Commands_Should_Execute_And_Return_Completed()
+    {
+        // arrange
+        RoverService service = new RoverService();
+        int x = 0;
+        int y = 0;
+        MarsRover rover = new MarsRover(x, y);
+        int height = 5;
+        int width = 5;
+        IReadOnlySet<Position> obstacles = new HashSet<Position>();
+        Plateau plateau = new Plateau(height, width, obstacles);
+        string commands = "mrmmll";
+        // act
+        var result = service.Execute(rover, plateau, commands);
+        // assert
+        result.Status.Should().Be(MoveStatus.Completed);
+        rover.Heading.Should().Be(Direction.West);
+        rover.Position.X.Should().Be(2);
+        rover.Position.Y.Should().Be(1);
+    }
+
+    [Fact]
+    public void When_Moving_OutOfBound_Should_Return_OutOfBound_And_Last_Postion()
+    {
+        // arrange
+        RoverService service = new RoverService();
+        int x = 3;
+        int y = 3;
+        MarsRover rover = new MarsRover(x, y);
+        int height = 5;
+        int width = 5;
+        IReadOnlySet<Position> obstacles = new HashSet<Position>();
+        Plateau plateau = new Plateau(height, width, obstacles);
+        string commands = "mm";
+        // act
+        var result = service.Execute(rover, plateau, commands);
+        // assert
+        result.Status.Should().Be(MoveStatus.OutOfBounds);
+        rover.Heading.Should().Be(Direction.North);
+        rover.Position.X.Should().Be(3);
+        rover.Position.Y.Should().Be(4);
+    }
+
+    [Fact]
+    public void When_Moving_To_An_Obstacle_Should_Return_Blocked_And_From_Position()
+    {
+        // arrange
+        int xObstacle = 3;
+        int yObstacle = 3;
+        Position obstacle = new Position(xObstacle, yObstacle);
+        IReadOnlySet<Position> obstacles = new HashSet<Position>() { obstacle };
+        RoverService service = new RoverService();
+        int x = 2;
+        int y = 3;
+        MarsRover rover = new MarsRover(x, y);
+        int height = 5;
+        int width = 5;
+        Plateau plateau = new Plateau(height, width, obstacles);
+        string commands = "rm";
+        // act
+        var result = service.Execute(rover, plateau, commands);
+        // assert
+        result.Status.Should().Be(MoveStatus.Blocked);
+        rover.Heading.Should().Be(Direction.East);
+        rover.Position.X.Should().Be(2);
+        rover.Position.Y.Should().Be(3);
+    }
+
 }
