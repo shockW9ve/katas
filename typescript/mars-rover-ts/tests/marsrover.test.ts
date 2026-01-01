8b25c24555a54b66107841da91216ffee08c8dc6
shockW9ve <shockW9ve@gmail.com>
2026-01-01 16:12:27 +0100

    test: comments for it.each tests

--- typescript/mars-rover-ts/tests/marsrover.test.ts
+++ typescript/mars-rover-ts/tests/marsrover.test.ts
@@ -3,6 +3,32 @@ import { MarsRover, RoverLocation } from "../src/models/marsrover.js";
 import { Position } from "../src/models/position.js";
 import { Direction } from "../src/helpers/direction.js";
 
+// test.each([
+//   { a: 1, b: 1, expected: 2 },
+//   { a: 1, b: 2, expected: 3 },
+//   { a: 2, b: 1, expected: 3 },
+// ])('add($a, $b) -> $expected', ({ a, b, expected }) => {
+//   expect(a + b).toBe(expected)
+// })
+
+// this will return
+// ✓ add(1, 1) -> 2
+// ✓ add(1, 2) -> 3
+// ✓ add(2, 1) -> 3
+
+// test.each([
+//   [1, 1, 2],
+//   [1, 2, 3],
+//   [2, 1, 3],
+// ])('add($0, $1) -> $2', (a, b, expected) => {
+//   expect(a + b).toBe(expected)
+// })
+
+// this will return
+// ✓ add(1, 1) -> 2
+// ✓ add(1, 2) -> 3
+// ✓ add(2, 1) -> 3
+
 describe("Mars Rover", () => {
   it("Report heading", () => {
     // arrange
--- typescript/mars-rover-ts/tests/plateau.test.ts
+++ typescript/mars-rover-ts/tests/plateau.test.ts
@@ -3,3 +3,29 @@ import { describe, it, expect } from "vitest";
 describe("Plateau", () => {
   it("Grid", () => {});
 });
+
+// test.each([
+//   { a: 1, b: 1, expected: 2 },
+//   { a: 1, b: 2, expected: 3 },
+//   { a: 2, b: 1, expected: 3 },
+// ])('add($a, $b) -> $expected', ({ a, b, expected }) => {
+//   expect(a + b).toBe(expected)
+// })
+
+// this will return
+// ✓ add(1, 1) -> 2
+// ✓ add(1, 2) -> 3
+// ✓ add(2, 1) -> 3
+
+// test.each([
+//   [1, 1, 2],
+//   [1, 2, 3],
+//   [2, 1, 3],
+// ])('add($0, $1) -> $2', (a, b, expected) => {
+//   expect(a + b).toBe(expected)
+// })
+
+// this will return
+// ✓ add(1, 1) -> 2
+// ✓ add(1, 2) -> 3
+// ✓ add(2, 1) -> 3
