79ba2c697015a27b980a7b361dbea1752ca91edd
shockW9ve <shockW9ve@gmail.com>
2026-01-15 20:03:00 +0100

    very tired small update in the mess

--- typescript/tennis/src/Game/TennisGame.ts
+++ typescript/tennis/src/Game/TennisGame.ts
@@ -34,8 +34,9 @@
 //         Matches are typically played as best-of-three or best-of-five sets.
 //         To win a match, a player must win the majority of the prescribed sets (2 out of 3 or 3 out of 5).
 //
-enum phase {
+enum Phase {
   Normal,
+  Fortyland,
   Deuce,
   Advantage,
   Game,
@@ -49,9 +50,9 @@ const INDEX: Record<number, string> = {
 };
 
 type State = {
-  playerA: string;
-  playerB: string;
-  phase: phase;
+  playerA: string | undefined;
+  playerB: string | undefined;
+  phase: Phase;
 };
 
 type PlayerId = "A" | "B";
@@ -87,30 +88,56 @@ export class Player {
   }
 }
 export default class TennisGame implements Tennis {
+  private pointA: number;
+  private pointB: number;
   private playerA: Player;
   private playerB: Player;
   private readonly points: Set<string>;
   score = ({ a, b }: Points) => `${a}-${b}`;
 
+  private phase: Phase;
+  private readonly normal: Map<number, string> = new Map([
+    [0, "0"],
+    [1, "15"],
+    [2, "30"],
+  ]);
+
   constructor(playerA: Player, playerB: Player, points: Array<Points>) {
-    this.playerA = playerA;
-    this.playerB = playerB;
-    this.points = new Set(points.map(this.score));
+    // this.playerA = playerA;
+    // this.playerB = playerB;
+    // this.points = new Set(points.map(this.score));
+    this.pointA = 0;
+    this.pointB = 0;
+    this.phase = Phase.Normal;
   }
 
   point(player: PlayerId) {
     if (player === "A") {
-      this.playerA.setPoint();
+      this.pointA++;
     } else {
-      this.playerB.setPoint();
+      this.pointB++;
     }
+    // if (player === "A") {
+    //   this.playerA.setPoint();
+    // } else {
+    //   this.playerB.setPoint();
+    // }
   }
 
   currentScore(): string {
-    return "";
+    return `${this.normal.get(this.pointA)}-${this.normal.get(this.pointB)}`;
   }
 
-  nextState(player: string, state: State): State {
-    return { playerA: "15", playerB: "0", phase: phase.Normal };
+  nextState(player: PlayerId, state: State): State {
+    if (player === "A") {
+      // TODO USE STATE
+      const nextScore = this.normal.get(this.pointA + 1);
+      const currentScore = this.normal.get(this.pointB);
+      return { playerA: nextScore, playerB: currentScore, phase: this.phase };
+    } else {
+      const nextScore = this.normal.get(this.pointB + 1);
+      const currentScore = this.normal.get(this.pointA);
+      return { playerA: currentScore, playerB: nextScore, phase: this.phase };
+    }
   }
 }
