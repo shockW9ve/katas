2992899979706d8f9af62532186ac39cdd26be97
shockW9ve <shockW9ve@gmail.com>
2026-02-22 18:02:22 +0100

    daily :)

--- typescript/ledger/src/ledger/ledger.ts
+++ typescript/ledger/src/ledger/ledger.ts
@@ -1,5 +1,26 @@
-export function initialState() {
-  let balance = 0;
+import { Command, Event, LedgerState } from "./types.js";
+export function initialState(): LedgerState {
+  return {
+    balance: 0,
+    events: [],
+  };
 }
-export function transition(state, command): { next; events } {}
+export function transition(
+  state: LedgerState,
+  command: Command,
+): { next; events } {
+  if (command.type === "Desposit") {
+    return {
+      next: { balance: state.balance + command.amount },
+      events: command.type,
+    };
+  }
+  return { next: "", events: [] };
+}
 export function apply(state, event) {}
+
+function evaluateEvent(event: Event): LedgerState {
+  if (event === "Desposit") {
+    return {};
+  }
+}
--- typescript/ledger/src/ledger/types.ts
+++ typescript/ledger/src/ledger/types.ts
@@ -3,5 +3,8 @@ export type Command = {
   amount: number;
 };
 export type Event = "Desposit" | "Withdraw" | "WithdrawalRejected";
-export type LedgerState = {};
+export type LedgerState = {
+  balance: number;
+  events: [];
+};
 export type Money = "USD" | "EUR" | "GBP";
