317a59ae79edc676bb57aa6763bf5d9666fdd445
shockW9ve <shockW9ve@gmail.com>
2026-02-24 19:00:14 +0100

    daily :) - next refactor code flow

--- typescript/ledger/src/ledger/ledger.ts
+++ typescript/ledger/src/ledger/ledger.ts
@@ -1,22 +1,50 @@
-import { Command, Event, LedgerState } from "./types.js";
+import { Action, ActionTaken, Command, Event, LedgerState } from "./types.js";
 export function initialState(): LedgerState {
   return {
     balance: 0,
-    // events: [],
+    events: [],
   };
 }
 export function transition(
   state: LedgerState,
   command: Command,
-  // ): { next: LedgerState; events: Array<Event> } {
-): { next: LedgerState; events: Array<Command> } {
+): { next: LedgerState; events: Array<ActionTaken> } {
   if (command.type === "Deposit") {
     return {
-      next: { balance: state.balance + command.amount },
-      events: [command],
+      next: { balance: state.balance + command.amount, events: [command.type] },
+      events: [{ type: "Deposited", amount: command.amount }],
     };
+  } else if (command.type === "Withdraw") {
+    let funds = isValid(state.balance, command.amount);
+    if (funds) {
+      return {
+        next: {
+          balance: state.balance - command.amount,
+          events: [command.type],
+        },
+        events: [{ type: "Withdrawn", amount: command.amount }],
+      };
+    } else {
+      return {
+        next: {
+          balance: state.balance,
+          events: [command.type],
+        },
+        events: [{ type: "WithdrawalRejected", amount: command.amount }],
+      };
+    }
   }
-  return { next: { balance: 0, events: [command.type] } };
+  return { next: { balance: 0, events: [] }, events: [] };
+}
+
+function balance(state: LedgerState) {}
+
+function isValid(balance: number, amount: number) {
+  if (balance - amount < 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
 // export function apply(state, event) {}
 //
--- typescript/ledger/src/ledger/types.ts
+++ typescript/ledger/src/ledger/types.ts
@@ -2,9 +2,15 @@ export type Command = {
   type: Event;
   amount: number;
 };
-export type Event = "Deposit" | "Withdraw" | "WithdrawalRejected";
+export type Event = "Deposit" | "Withdraw";
+export type Action =
+  | "Deposited"
+  | "Withdrawn"
+  | "WithdrawalRejected"
+  | "InvalidAction";
+export type ActionTaken = { type: Action; amount: number };
 export type LedgerState = Readonly<{
   balance: number;
-  // events: Array<Event>;
+  events: Array<Event>;
 }>;
 export type Money = "USD" | "EUR" | "GBP";
