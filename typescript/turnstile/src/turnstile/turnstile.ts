510b49ee7980ed0f0077cff2a8310964062caf6c
shockW9ve <shockW9ve@gmail.com>
2026-03-03 20:02:31 +0100

    ternary test

--- typescript/turnstile/src/turnstile/turnstile.ts
+++ typescript/turnstile/src/turnstile/turnstile.ts
@@ -5,16 +5,16 @@ export function transition(
   event: Event,
 ): { nextState: State; actions: Actions[] } {
   if (state === "Locked") {
-    // let status =
-    //   event === "Coin"
-    //     ? { nextState: "Unlocked", actions: ["ThankYou"] }
-    //     : { nextState: "Locked", actions: ["Alarm"] };
-    // return status;
-    if (event === "Coin") {
-      return { nextState: "Unlocked", actions: ["ThankYou"] };
-    } else {
-      return { nextState: "Locked", actions: ["Alarm"] };
-    }
+    let status: { nextState: State; actions: Actions[] } =
+      event === "Coin"
+        ? { nextState: "Unlocked", actions: ["ThankYou"] }
+        : { nextState: "Locked", actions: ["Alarm"] };
+    return status;
+    // if (event === "Coin") {
+    //   return { nextState: "Unlocked", actions: ["ThankYou"] };
+    // } else {
+    //   return { nextState: "Locked", actions: ["Alarm"] };
+    // }
   } else {
     if (event === "Coin") {
       return { nextState: "Unlocked", actions: ["ReturnCoin"] };
