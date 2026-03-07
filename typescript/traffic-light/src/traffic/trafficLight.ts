feec9f00a0aedca252e92f8533fc7cd4d0f7ff98
shockW9ve <shockW9ve@gmail.com>
2026-03-07 12:36:07 +0100

    Green tests

--- typescript/traffic-light/src/traffic/trafficLight.ts
+++ typescript/traffic-light/src/traffic/trafficLight.ts
@@ -8,7 +8,29 @@ export function transition(
   state: LightState,
   event: LightEvent,
 ): TransitionResult {
-  return { nextState: "OFF", action: [] };
+  if (event.type === "Emergency") {
+    let action: LightAction = { action: "PauseLight", to: "Red" };
+    return { nextState: action.to, actions: [action] };
+  }
+  if (state === "Red") {
+    let result: LightAction =
+      state === "Red" && event.type === "TimerElapsed"
+        ? { action: "SetLight", to: "Green" }
+        : { action: "PauseLight", to: state };
+    return { nextState: result.to, actions: [result] };
+  } else if (state === "Yellow") {
+    let result: LightAction =
+      state === "Yellow" && event.type === "TimerElapsed"
+        ? { action: "SetLight", to: "Red" }
+        : { action: "PauseLight", to: state };
+    return { nextState: result.to, actions: [result] };
+  } else {
+    let result: LightAction =
+      state === "Green" && event.type === "TimerElapsed"
+        ? { action: "SetLight", to: "Yellow" }
+        : { action: "PauseLight", to: state };
+    return { nextState: result.to, actions: [result] };
+  }
 }
 
 export function initialState(): LightState {
--- typescript/traffic-light/src/traffic/types.ts
+++ typescript/traffic-light/src/traffic/types.ts
@@ -1,5 +1,9 @@
 export type LightState = "Green" | "Yellow" | "Red" | "OFF";
-export type LightEvent = "TimerElapsed" | "Emergency";
-export type Action = "SetLight" | "TurnOffLights";
+export type Event = "TimerElapsed" | "Emergency";
+export type LightEvent = { type: Event };
+export type Action = "SetLight" | "PauseLight" | "TurnOffLights";
 export type LightAction = { action: Action; to: LightState };
-export type TransitionResult = { nextState: LightState; action: Action[] };
+export type TransitionResult = {
+  nextState: LightState;
+  actions: LightAction[];
+};
