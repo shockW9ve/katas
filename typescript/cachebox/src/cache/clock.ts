d2e50069d0ae330270f6e7fd48977a284424dc50
shockW9ve <shockW9ve@gmail.com>
2026-04-02 17:36:24 +0200

    simply clock for normal and test

--- typescript/cachebox/src/cache/clock.ts
+++ typescript/cachebox/src/cache/clock.ts
@@ -1,49 +1,19 @@
 export class Clock {
   private dateInit: Date;
-  private expire: number;
 
-  constructor(init: number) {
+  constructor() {
     this.dateInit = new Date();
-    this.expire = init;
   }
 
   initDate() {
     return this.dateInit;
   }
 
-  timeNow(): number {
-    const date: Date = new Date();
-    const ms: number = date.getTime();
-    return ms;
-  }
-
-  calculateExpire(time: number, ttl?: number): number {
-    if (!ttl) {
-      return time;
-    }
-    this.expire = time + ttl;
-    return time + ttl;
-  }
-
-  // msNow(): number {
-  //   const dateNow: Date = new Date();
-  //   const ms: number = dateNow.getTime();
-  //   const time: number = ms - this.dateInit.getTime();
-  //   return time;
-  // }
-  //
-  advanceMs(ttl: number): string {
-    const current = this.expire;
-    this.expire = this.expire - ttl;
-    if (this.expire <= current) {
-      return "evict";
-    } else {
-      return "safe";
-    }
-    // const dateNow: Date = new Date();
-    // const ms: number = dateNow.getTime();
-    // const time: number = ms - ttl;
-    // return time;
+  msNow(): number {
+    const dateNow: Date = new Date();
+    const ms: number = dateNow.getTime();
+    const time: number = ms - this.dateInit.getTime();
+    return time;
   }
 
   toString() {
@@ -52,30 +22,13 @@ export class Clock {
 }
 
 export class FakeClock {
-  private ttl: number;
-  // private start: number;
+  private ttlStart: number;
 
   constructor(ttl: number) {
-    this.ttl = ttl;
-    // this.start = ttl;
+    this.ttlStart = ttl;
   }
 
-  // advanceMs(tick: number, cache: CacheBox<string, number>, key: string) {
-  //   this.ttl = this.ttl - tick;
-  //   if (this.ttl <= 0) {
-  //     cache?.delete(key);
-  //   }
-  // }
-  //
-  // set timer(time: number) {
-  //   this.ttl = time;
-  // }
-  //
-  // get timer() {
-  //   return this.ttl;
-  // }
-  //
-  // get starter() {
-  //   return this.start;
-  // }
+  advanceMs(ttl: number): void {
+    this.ttlStart = this.ttlStart - ttl;
+  }
 }
