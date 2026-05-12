efb15f6ac99d020f1f148a58cea4ea046663d385
shockW9ve <shockW9ve@gmail.com>
2026-05-12 19:14:58 +0200

    wrong branch but start of a small kata

---
+++ typescript/freeCodeCamp/src/matrix.ts
@@ -0,0 +1,17 @@
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+];
+
+function transpose(matrix: Array<number>): Array<number> {
+  const transposedMatrix: Array<number> = [];
+
+  if (matrix.length < 1) {
+    return [];
+  }
+
+  for (let numbers of matrix) {
+  }
+
+  return transposedMatrix;
+}
